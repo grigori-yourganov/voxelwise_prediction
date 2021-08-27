5c615f8fa4385eb8723de4a1e50160ac152da2c9
grigori-yourganov <61881538+grigori-yourganov@users.noreply.github.com>
2021-08-27 16:24:21 -0400

    Update qsub_chunk.sh

--- qsub_chunk.sh
+++ qsub_chunk.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#
 #PBS -l select=1:ncpus=30:mem=300gb:interconnect=hdr
 #PBS -l walltime=72:00:00
 #PBS -j oe
 
-matlab_input="process_chunk("$chunk","$gm")"
+matlab_input="process_chunk("$chunk")"
 echo $matlab_input
-output_log=$HOME"/source/predict_voxels/log_chunk"$chunk"_"$gm
+output_log=$HOME"/source/predict_voxels/log_chunk"$chunk
 module load matlab/2020a
 cd $HOME/source/predict_voxels
 matlab -nodisplay -nosplash -r ${matlab_input} -logfile ${output_log}
