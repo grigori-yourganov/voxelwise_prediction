3c919206654f042f06a2f5ca00fd3a0b65888707
grigori-yourganov <61881538+grigori-yourganov@users.noreply.github.com>
2021-08-27 16:23:13 -0400

    Update main_batch.sh

--- main_batch.sh
+++ main_batch.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 for chunk in {1..8}; do
-  jobname="chunk"$chunk"_GM"
+  jobname="chunk"$chunk"_i3m"
   echo $jobname
   cd $HOME/source/predict_voxels
-  qsub -N $jobname -v chunk=$chunk,gm=1 qsub_chunk.sh
-done
-
-for chunk in {1..8}; do
-  jobname="chunk"$chunk"_WM"
-  echo $jobname
-  cd $HOME/source/predict_voxels
-  qsub -N $jobname -v chunk=$chunk,gm=0 qsub_chunk.sh
+  qsub -N $jobname -v chunk=$chunk qsub_chunk.sh
 done
