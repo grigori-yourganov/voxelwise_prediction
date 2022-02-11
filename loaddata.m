1964debe43668c5f48b218e881e22d05751bbdf5
grigori-yourganov <61881538+grigori-yourganov@users.noreply.github.com>
2022-02-11 15:32:32 -0500

    Update loaddata.m

--- loaddata.m
+++ loaddata.m
@@ -1,9 +1,12 @@
 clear all
 % specify these variables:
 % the exact name of the voxelwise modality ('i3mT1', 'fa', 'md', etc)
-modality = 'i3mT1';
+modality = 'fa';
 % the path to the MAT files
 path = 'C:\Users\gyourga\Documents\data\POLAR_FIRST_SESSION_ONLY_MAT_FILES';
+% the paths to NiiStat and SPM
+addpath ('C:/Users/gyourga/Documents/code/NiiStat-master/');
+addpath ('C:/Users/gyourga/Documents/code/spm12/');
 
 warning ('off', 'MATLAB:unknownElementsNowStruc');
 cd (path);
@@ -13,15 +16,25 @@ hdr = [];
 for i = 1:length (d)
     l = load (d(i).name);
     if isfield (l, modality)
-        neurodata (:, :, :, idx) = l.i3mT1.dat;
-        subj_name{idx} = d(i).name;
         if isempty (hdr)
-            hdr = l.i3mT1.hdr;
+           % first subject will provide the reference header
+           hdr = l.(modality).hdr;
+           neurodata (:, :, :, idx) = l.(modality).dat;
+        else
+           if sum (hdr.dim ~= l.(modality).hdr.dim) == 0 % no need to reslice
+               neurodata (:, :, :, idx) = l.(modality).dat;
+           else % need to reslice to the first subject
+               [new_hdr, new_dat] = nii_reslice_target(l.(modality).hdr, l.(modality).dat, hdr); 
+               neurodata (:, :, :, idx) = new_dat;
+           end
         end
+        
+        subj_name{idx} = d(i).name;
+
         idx = idx + 1;
     else
         disp ([d(i).name ' does not have ' modality]);
     end
 end
 
-save ([modality '.mat'], 'neurodata', 'subj_name', 'hdr');
+save ([modality '.mat'], 'neurodata', 'subj_name', 'hdr', '-v7.3');
