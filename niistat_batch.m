a0bd0d55a4c8ea5b5160e50b5c4692d643241ef2
grigori-yourganov <61881538+grigori-yourganov@users.noreply.github.com>
2022-03-30 15:30:48 -0400

    Add files via upload

---
+++ niistat_batch.m
@@ -0,0 +1,7 @@
+xlsname = 'C:\Users\gyourga\Documents\data\POLAR_FIRST_SESSION_ONLY_MAT_FILES\behavioral_lj.xlsx';
+roiIndices = 1;
+modalityIndices = 8;
+numPermute = -1;
+pThresh = 0.05;
+minOverlap = 5;
+NiiStat(xlsname, roiIndices, modalityIndices,numPermute, pThresh, minOverlap);
